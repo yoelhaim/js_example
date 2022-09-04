9cf266a90aa6ba76b7f17593a42b4b1458701896
yoelhaim <youssefelhaimer8@gmail.com>
2022-09-04 23:53:09 +0100

    add comment and fixed  loop :rocket:

--- random_char.js
+++ random_char.js
@@ -1,9 +1,12 @@
 function rand( max)
 {
-	let c = "abcdefghijkmnopqrstuvxywz";
-	let s=[];
-	for(let i = 0; i <= max; i++)
-		s.push(c[Math.floor(Math.random() * 26)]);
-	return (s.join(''));
+	let c = "abcdefghijkmnopqrstuvxywz";// char a to z
+	let s=[]; // array 
+	for(let i = 0; i < max; i++) // for loop max
+		s.push(c[Math.floor(Math.random() * 26)]); // push random index 0 to 25
+	return (s.join('')); // return result
 }
-console.log(rand(10))
+console.log(rand(10))// printf result 
+console.log(rand(5))// printf result 
+console.log(rand(2))// printf result 
+console.log(rand(50))// printf result 
